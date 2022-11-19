ec1b011725e695fe8bfc0de2f598bdee4c5f5288
Ainhoaleft <izquierdo19668@iesmarenostrum.com>
2022-11-19 17:07:33 +0100

    CRUD

--- MvcAgenda2/MvcAgenda/Migrations/20221117174824_Inicial.Designer.cs
+++ MvcAgenda2/MvcAgenda/Migrations/20221117174824_Inicial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcAgenda.Data;
 
@@ -11,9 +12,10 @@ using MvcAgenda.Data;
 namespace MvcAgenda.Migrations
 {
     [DbContext(typeof(MvcAgendaContexto))]
-    partial class MvcAgendaContextoModelSnapshot : ModelSnapshot
+    [Migration("20221117174824_Inicial")]
+    partial class Inicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -50,17 +52,11 @@ namespace MvcAgenda.Migrations
                     b.Property<int>("DepartamentoId")
                         .HasColumnType("int");
 
-                    b.Property<string>("Email")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime?>("FechaNacimiento")
+                    b.Property<DateTime>("FechaNacimiento")
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Nombre")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Telefono")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
